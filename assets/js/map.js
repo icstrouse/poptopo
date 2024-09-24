333407cc40ae503796c5d068c0e10d46291849b2
Ian Strouse <ian.strouse@gmail.com>
2024-09-23 20:23:34 -0600

    Make markers dynanmic

--- assets/js/map.js
+++ assets/js/map.js
@@ -2,6 +2,8 @@
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiaXN0cm91c2UiLCJhIjoiY20weDA1anBrMDdpOTJpcHNibW5ibnNqdiJ9.E3oVQIz8a3w4Dg1GQ_Ki-w';
 
+const tags = JSON.parse(document.getElementById('map').getAttribute('tags'));
+
 const map = new mapboxgl.Map({
   container: 'map',
   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
@@ -11,9 +13,16 @@ const map = new mapboxgl.Map({
 });
 
 // Tag
-const marker1 = new mapboxgl.Marker()
-  .setLngLat([-105, 40])
-  .addTo(map);
+const markers = tags.map(tag =>
+  new mapboxgl.Marker({
+    color: "#555555",
+    draggable: true
+  })
+    .setLngLat([parseFloat(tag.lng), parseFloat(tag.lat)])
+    .setPopup(new mapboxgl.Popup().setHTML(`<h1>${tag.name}</h1>`))
+    .addTo(map)
+);
+console.log(markers);
 
 // Tracks
 map.on('load', () => {
@@ -38,7 +47,7 @@ map.on('load', () => {
       'line-cap': 'round'
     },
     paint: {
-      'line-color': '#888',
+      'line-color': '#555555',
       'line-width': 8
     }
   });
--- lib/poptopo_web/controllers/map_controller.ex
+++ lib/poptopo_web/controllers/map_controller.ex
@@ -1,7 +1,20 @@
 defmodule PoptopoWeb.MapController do
   use PoptopoWeb, :controller
 
+  alias Poptopo.Maps
+  alias Poptopo.Maps.Tag
+
   def index(conn, _params) do
-    render(conn, :index)
+    {:ok, tags} = Jason.encode(for(tag <- Maps.list_tags(), do: data(tag)))
+    render(conn, :index, tags: tags )
+  end
+
+  defp data(%Tag{} = tag) do
+    %{
+      name: tag.name,
+      user_id: tag.user_id,
+      lat: tag.lat,
+      lng: tag.lng
+    }
   end
 end
