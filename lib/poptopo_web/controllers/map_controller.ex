5f8ed4c44dff57fed8f51f1eb134770cb97c4665
Ian Strouse <ian.strouse@gmail.com>
2024-09-27 15:34:46 -0600

    Get tracks from DB

--- lib/poptopo_web/controllers/map_controller.ex
+++ lib/poptopo_web/controllers/map_controller.ex
@@ -3,18 +3,29 @@ defmodule PoptopoWeb.MapController do
 
   alias Poptopo.Maps
   alias Poptopo.Maps.Tag
+  alias Poptopo.Maps.Track
 
   def index(conn, _params) do
-    {:ok, tags} = Jason.encode(for(tag <- Maps.list_tags(), do: data(tag)))
-    render(conn, :index, tags: tags )
+    {:ok, tags} = Jason.encode(for(tag <- Maps.list_tags(), do: parseTag(tag)))
+    {:ok, tracks} = Jason.encode(for(track <- Maps.list_tracks(), do: parseTrack(track)))
+
+    render(conn, :index, tags: tags, tracks: tracks)
   end
 
-  defp data(%Tag{} = tag) do
+  defp parseTag(%Tag{} = tag) do
     %{
       name: tag.name,
       user_id: tag.user_id,
       lat: tag.lat,
       lng: tag.lng
+    }
+  end
+
+  defp parseTrack(%Track{} = track) do
+    %{
+      name: track.name,
+      user_id: track.user_id,
+      data: track.data
     }
   end
 end
